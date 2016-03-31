69e4a7b8eb93ae91ef96ed555eb4d3bdaf07be59
leftygray <leftygray@gmail.com>
2016-03-31 15:05:54 +1100

    Shifted distribution plotting into another Bayes functions

--- code/BayesFunctions.R
+++ code/BayesFunctions.R
@@ -5,6 +5,9 @@
 # This script contains functions useful for the cascade incidence analysis
 # and results and figures generation. 
 
+# Analysis Functions
+# ==================
+
 incFunc <- function(cascadeData, beta1, beta2, beta3, beta4) {
   # Function for can easily calculating the incidence values
   # for each sample of the beta values
@@ -49,4 +52,69 @@ WeightError <- function(dataValues, estimatedValues, dataError) {
   # Calculate the weight assuming a normal likelihood
   weight <- prod(dnorm(distance, mu, sigma) / dnorm(mu, mu, sigma))
   return(weight)
+}
+
+# Plotting Functions
+# ==================
+
+parameterPlot <- function(paramter, priorsSamples, posteriorSamples, 
+                          save = FALSE) {
+  # This function generates a plot of the prior and posterior for each
+  # parameter. Specificly set up for this project. Parameters and inputs 
+  # must correspond to the incidence model.
+  #
+  # Args:
+  #   parameter: Model input paramter to plot. 
+  #     Only beta1, beta2, beta3, beta4 allowed
+  #   priorSamples: Data frame with columns corresponding to samples from 
+  #     the prior parameter distributions
+  #   posteriorSamples: Data frame with columns corresponding to samples from 
+  #     the posterior parameter distributions
+  #   save: Set to true if you want to save the plot
+  # Returns:
+  #   Plot handle corresponding to the created plot
+  #
+  # ----------------------------------------------------------------------
+  
+  # Check input paramter is appropriate
+  if (!(parameter %in% c("beta1", "beta2", "beta3", "beta4"))) {
+    stop("Unknown parameter entered")
+  }
+  
+  # Specify parameter labels
+  labels <- c("beta1" = "Undiagnosed Beta",
+              "beta2" = "Diagnosed Beta",
+              "beta3" = "Unsuppressed Beta",
+              "beta4" = "Suppressed Beta")
+  
+  priorDist <- priorFrame[, dist]
+  postDist <- posteriorFrame[, dist]
+  plotRange <- range(priorDist)
+  plotStats <- paste("Mean, median, mode:   \n", 
+                     "prior = ", 
+                     toString(signif(mean(priorDist), digits = 2)), ", ",
+                     toString(signif(median(priorDist), digits = 2)), ", ",
+                     toString(signif(getmode(priorDist), digits = 2)), "   \n", 
+                     "postior = ", 
+                     toString(signif(mean(postDist), digits = 2)), ", ",
+                     toString(signif(median(postDist), digits = 2)), ", ",
+                     toString(signif(getmode(postDist), digits = 2)), "   \t\n", 
+                     sep = "")
+  
+  postPlot <- ggplot(data = posteriorFrame, aes_string(x = dist)) +
+    geom_density(data = priorFrame, fill = "black", alpha = 0.2) +
+    geom_density(colour = "red", fill = "red", alpha = 0.1) +
+    coord_cartesian(xlim = plotRange) +
+    ylab("Density") + 
+    xlab(labels[dist]) +
+    annotate("text", label = plotStats, 
+             x = Inf, y = Inf, hjust = 1, vjust = 1) +
+    plotOpts
+  
+  if (save) {
+    ggsave(file.path(resultsFolder, 
+                     paste("parameter_distribution-", paramter, ".png",sep ="")),
+           plot = postPlot, width = 12, height = 10, 
+           units = "cm")
+  }
 }
