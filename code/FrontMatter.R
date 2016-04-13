12abbfc434a41d9904ff6f0a510a21e65c1bf63b
leftygray <leftygray@gmail.com>
2016-04-13 22:07:51 +1000

    Finished FrontMatter function

--- code/FrontMatter.R
+++ code/FrontMatter.R
@@ -1,18 +1,32 @@
 ## FrontMatter.R
 
-# This personal function is used to create a string of information regarding 
-# the use version and package details for printing in documents.
+# This personal function is used to create a string of information 
+# regarding the use version and package details for printing in documents.
 
 # Richard T. Gray
 
 FrontMatter <- function(packages = NULL, rstudio = NULL) {
+  # This function produces a string with useful information about the tools 
+  # used for analysis. The returned string includes newline characters so 
+  # requires cat() to display properly.
+  
+  # Args:
+  #   packages: List of packages we want to display. Default is NULL saying
+  #     we don't want to return any package information.
+  #   rstudio: String specifying Rstudio version used. Default is NULL.
+  #
+  # Returns
+  #   A string with the front matter information. By default returns R 
+  #     version and time.
+  #
+  # -----------------------------------------------------------------------
   
   # String materials
   versionStr <- R.Version()$version
   time <- format(Sys.Date(), format="%Y-%m-%d")
   
   # Setup R/Rstudio string
-  rstr <- paste("Version info: Code for this page was tested in", 
+  rstr <- paste("Version info: Code run in", 
                 versionStr)
   if (!is.null(rstudio)) {
     rstr <- paste(rstr, "\nUsing: Rstudio version", rstudio)
@@ -23,11 +37,14 @@ FrontMatter <- function(packages = NULL, rstudio = NULL) {
   
   # Setup package string
   if (!is.null(packages)) {
-    # loop through 
+    # loop through inputed list of packages
+    pstr <- "With: "
+    for (pkg in packages) {
+      pstr <- paste(pstr, pkg, " ", packageVersion(pkg), "; ", sep = "")
+    }
   }
   
-  # Put everything together
-  finalStr <- paste(rstr, "\n", tstr, sep = "")
-  
+  # Put everything together and return
+  finalStr <- paste(rstr, "\n", tstr, "\n", pstr, sep = "")
   return(finalStr)
 }
